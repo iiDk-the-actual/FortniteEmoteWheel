5d1d059e8a64e54ea51dd45847d9f496c393a93b
Grayson <Grayson@Desktop-7F7HF>
2025-01-29 18:59:47 -0500

    Change plugininfo

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -7,6 +7,6 @@
     {
         public const string GUID = "com.goldentrophy.gorillatag.fortniteemotewheel";
         public const string Name = "FortniteEmoteWheel";
-        public const string Version = "1.1.4";
+        public const string Version = "1.1.5";
     }
 }
