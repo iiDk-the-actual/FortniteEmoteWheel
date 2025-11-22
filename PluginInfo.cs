ad694760c5e6d314617e5c094901aed04041a974
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-11-22 00:52:41 -0500

    Update console

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -7,6 +7,6 @@
     {
         public const string GUID = "com.goldentrophy.gorillatag.fortniteemotewheel";
         public const string Name = "FortniteEmoteWheel";
-        public const string Version = "1.3.2";
+        public const string Version = "1.3.3";
     }
 }
